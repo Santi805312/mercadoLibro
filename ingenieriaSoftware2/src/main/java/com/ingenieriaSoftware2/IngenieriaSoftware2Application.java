944e85ce1ca41c1790379a965b85813f32642be2
Santi805312 <sonandi1503@gmail.com>
2026-08-28 08:53:19 -0300

    Primer commit

---
+++ ingenieriaSoftware2/src/main/java/com/ingenieriaSoftware2/IngenieriaSoftware2Application.java
@@ -0,0 +1,13 @@
+package com.ingenieriaSoftware2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngenieriaSoftware2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngenieriaSoftware2Application.class, args);
+	}
+
+}
---
+++ ingenieriaSoftware2/src/test/java/com/ingenieriaSoftware2/IngenieriaSoftware2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenieriaSoftware2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngenieriaSoftware2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
