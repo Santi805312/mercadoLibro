950632002b2bb619c8d7d99b079fa01f1272cb59
Santi805312 <sonandi1503@gmail.com>
2026-09-02 09:52:29 -0300

    feat: agregar entidad Categoria

---
+++ ingenieriaSoftware2/src/main/java/com/ingenieriaSoftware2/entity/Categoria.java
@@ -0,0 +1,11 @@
+package com.ingenieriaSoftware2.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity @Table(name="categoria")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Categoria {
+    @Id @Column(name="nombre", nullable=false) private String nombre;
+    @Column(name="categoria_padre_id") private String categoriaPadreId;
+}
