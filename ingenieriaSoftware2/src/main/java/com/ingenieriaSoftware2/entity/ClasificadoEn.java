c1dc2f9a37e40a4722edf050428221a6a083275b
Santi805312 <sonandi1503@gmail.com>
2026-09-02 09:59:04 -0300

    fix: mapear CLASIFICADO_EN exactamente

--- ingenieriaSoftware2/src/main/java/com/ingenieriaSoftware2/entity/ClasificadoEn.java
+++ ingenieriaSoftware2/src/main/java/com/ingenieriaSoftware2/entity/ClasificadoEn.java
@@ -3,10 +3,10 @@ package com.ingenieriaSoftware2.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity @Table(name="CLASIFICADO_EN") @IdClass(ClasificadoEn.Id.class)
+@Entity @Table(name="CLASIFICADO_EN", schema="public") @IdClass(ClasificadoEn.Id.class)
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class ClasificadoEn {
-    @Id private String isbn;
-    @Id @Column(name="nombre_categoria") private String nombreCategoria;
+    @Id @Column(name="isbn", nullable=false) private String isbn;
+    @Id @Column(name="nombre_categoria", nullable=false) private String nombreCategoria;
     @Data @NoArgsConstructor @AllArgsConstructor public static class Id implements java.io.Serializable { String isbn; String nombreCategoria; }
 }
