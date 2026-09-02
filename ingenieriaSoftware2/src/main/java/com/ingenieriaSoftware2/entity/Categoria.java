e91325f73f806309348a187637e7ca93ed4bbd82
Santi805312 <sonandi1503@gmail.com>
2026-09-02 09:58:59 -0300

    fix: mapear Categoria a Supabase

--- ingenieriaSoftware2/src/main/java/com/ingenieriaSoftware2/entity/Categoria.java
+++ ingenieriaSoftware2/src/main/java/com/ingenieriaSoftware2/entity/Categoria.java
@@ -3,7 +3,7 @@ package com.ingenieriaSoftware2.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity @Table(name="categoria")
+@Entity @Table(name="categoria", schema="public")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Categoria {
     @Id @Column(name="nombre", nullable=false) private String nombre;
