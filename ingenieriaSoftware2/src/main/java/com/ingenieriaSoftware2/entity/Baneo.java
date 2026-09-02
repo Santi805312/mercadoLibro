cd7c39d277f2c0077975fcd867f55ddb5083652e
Santi805312 <sonandi1503@gmail.com>
2026-09-02 09:58:32 -0300

    fix: mapear Baneo exactamente

--- ingenieriaSoftware2/src/main/java/com/ingenieriaSoftware2/entity/Baneo.java
+++ ingenieriaSoftware2/src/main/java/com/ingenieriaSoftware2/entity/Baneo.java
@@ -4,12 +4,12 @@ import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
 
-@Entity @Table(name="baneo") @IdClass(Baneo.Id.class)
+@Entity @Table(name="baneo", schema="public") @IdClass(Baneo.Id.class)
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class Baneo {
-    @Id @Column(name="email_reportante_id") private String emailReportanteId;
-    @Id @Column(name="hora_reporte") private LocalDateTime horaReporte;
-    @Id @Column(name="email_reportado_id") private String emailReportadoId;
+    @Id @Column(name="email_reportante_id", nullable=false) private String emailReportanteId;
+    @Id @Column(name="hora_reporte", nullable=false) private LocalDateTime horaReporte;
+    @Id @Column(name="email_reportado_id", nullable=false) private String emailReportadoId;
     @Column(name="Fecha_inicio", nullable=false) private LocalDateTime fechaInicio;
     @Column(name="Fecha_fin") private LocalDateTime fechaFin;
     @Data @NoArgsConstructor @AllArgsConstructor public static class Id implements java.io.Serializable { String emailReportanteId; LocalDateTime horaReporte; String emailReportadoId; }
